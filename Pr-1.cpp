93addba00422d542e3fb3701697c9f2c0f467dd7
kavy009 <chauhankavya9116@gmail.com>
2025-01-20 16:01:19 +0530

    Add files via upload

---
+++ Pr-1.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+
+using namespace std;
+
+class Account_info
+{
+public:
+
+    string name;
+    int account_no,amount,withdrawn_amt;
+    float balance;
+
+
+    void deposit(int amount)
+    {
+        cout<<"Deposited amount is "<<amount<<endl;
+    }
+
+    void withdraw(int withdrawn_amt)
+    {
+
+        if (withdrawn_amt>amount)
+        {
+            cout<<"You have not sufficient Balance";
+        }
+
+
+        cout<<"Withdrawn amount is "<<withdrawn_amt<<endl;
+    }
+
+    void display()
+    {
+        balance=amount-withdrawn_amt;
+
+        cout<<"Remaining Balance is "<<balance<<endl;
+    }
+
+};
+
+int main()
+{
+    Account_info a1;
+
+     cout<<"Enter your Name ";
+     cin>>a1.name;
+      cout<<"Enter your Account no. ";
+     cin>>a1.account_no;
+      cout<<"Enter Amount to deposit ";
+     cin>>a1.amount;
+     a1.deposit(a1.amount);
+      cout<<"Enter Amount to withdraw ";
+     cin>>a1.withdrawn_amt;
+
+     a1.withdraw(a1.withdrawn_amt);
+
+     a1.display();
+
+
+}

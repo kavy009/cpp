398847b76d17515ff6b465b7a8375540d6c50ff0
kavy009 <chauhankavya9116@gmail.com>
2025-02-28 16:18:28 +0530

    Pr-2.5.cpp

--- Pr2.5.cpp
+++ Pr2.5.cpp
@@ -1,18 +1,40 @@
 #include<iostream>
+#include <math.h>
 using namespace std;
 
 class Loan_details
 {
     string customer_name;
     float loan_amt,rate,tenure,EMI;
-   
+
 public:
 
     void add_data()
     {
-        
+        cout<<"Enter loan applicant name: ";
+        cin>>customer_name;
+        cout<<"Enter loan amount:";
+        cin>>loan_amt;
+        cout<<"Enter interest rate:";
+        cin>>rate;
+        cout<<"Enter the tenure of loan in months: ";
+        cin>>tenure;
+
+        calculate(loan_amt,rate,tenure);
+
+  }
+
+    void calculate(float loan_amt, float rate, float tenure)
+    {
+        EMI= (loan_amt*rate*pow(1+rate,tenure))/(pow(1+rate,tenure)-1);
 
     }
+
+    void display()
+    {
+        cout<<"";
+    }
+
 
 };
 
@@ -20,33 +42,34 @@ public:
 
 int main()
 {
-    Loan_details L;
-    string customer_name;
-    float loan_amt,rate,tenure,EMI;
+    Loan_details l;
+
     int choice;
     do{
-        cout<<"Enter 1 to add details ,2 to dispaly detalis and 3 to exit";
+        cout<<"Enter 1 to add details ,2 to dispaly detalis and 0 to exit";
         cin>>choice;
 
         switch (choice)
         {
         case 1:
-        cout<<"Enter loan applicant name: ";
-        cin>>customer_name;
-        cout<<"Enter loan amount:";
-        cin>>loan_amt;
-        cout>>"Enter interest rate:";
-        cin>>rate;
-        cout<<"Enter the tenure of loan in months: ";
-        cin>>tenure;
-        
+             l.add_data();
+             break;
 
-            break;
-        
+        case 2:
+             l.display();
+              break ;
+
+        case 3:
+        cout<<"Exiting from the system...";
+
         default:
             break;
         }
 
     }
+    while(choice!=0);
+
+    return 0;
+
 
 }

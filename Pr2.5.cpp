aaf75791d8fca21e2b74946fadf4d85dadc30013
kavy009 <chauhankavya9116@gmail.com>
2025-04-08 22:23:39 +0530

    Pr2.5.cpp

--- Pr2.5.cpp
+++ Pr2.5.cpp
@@ -1,75 +1,74 @@
 #include<iostream>
-#include <math.h>
+#include<math.h>
 using namespace std;
 
-class Loan_details
-{
+class Loan_details {
     string customer_name;
-    float loan_amt,rate,tenure,EMI;
+    float loan_amt, rate, tenure, EMI;
 
 public:
 
-    void add_data()
-    {
-        cout<<"Enter loan applicant name: ";
-        cin>>customer_name;
-        cout<<"Enter loan amount:";
-        cin>>loan_amt;
-        cout<<"Enter interest rate:";
-        cin>>rate;
-        cout<<"Enter the tenure of loan in months: ";
-        cin>>tenure;
+    void add_data() {
+        cout << "Enter loan applicant name: ";
+        getline(cin, customer_name);
 
-        calculate(loan_amt,rate,tenure);
+        cout << "Enter loan amount: ";
+        cin >> loan_amt;
 
-  }
+        cout << "Enter annual interest rate (in %): ";
+        cin >> rate;
 
-    void calculate(float loan_amt, float rate, float tenure)
-    {
-        EMI= (loan_amt*rate*pow(1+rate,tenure))/(pow(1+rate,tenure)-1);
+        cout << "Enter the tenure of loan in months: ";
+        cin >> tenure;
 
+        calculate(loan_amt, rate, tenure);
     }
 
-    void display()
-    {
-        cout<<"";
+    void calculate(float loan_amt, float rate, float tenure) {
+
+        float R = (rate / 100) / 12;
+        float T = tenure;
+
+        EMI = (loan_amt * R * pow(1 + R, T)) / (pow(1 + R, T) - 1);
     }
 
-
+    void display() {
+        cout << "\n------ Loan Details ------" << endl;
+        cout << "Customer Name: " << customer_name << endl;
+        cout << "Loan Amount: " << loan_amt<< endl;
+        cout << "Annual Interest Rate: " << rate << "%" << endl;
+        cout << "Loan Tenure: " << tenure << " months" << endl;
+        cout << "Monthly EMI: " << EMI << endl;
+    }
 };
 
-
-
-int main()
-{
+int main() {
     Loan_details l;
-
     int choice;
-    do{
-        cout<<"Enter 1 to add details ,2 to dispaly detalis and 0 to exit";
-        cin>>choice;
 
-        switch (choice)
-        {
-        case 1:
-             l.add_data();
-             break;
+    do {
+        cout << "\nEnter:\n 1 - Add Loan Details\n 2 - Display Loan Details\n 0 - Exit\n";
+        cout << "Your choice: ";
+        cin >> choice;
 
-        case 2:
-             l.display();
-              break ;
+        switch (choice) {
+            case 1:
+                l.add_data();
+                break;
 
-        case 3:
-        cout<<"Exiting from the system...";
+            case 2:
+                l.display();
+                break;
 
-        default:
-            break;
+            case 0:
+                cout << "Exiting from the system..." << endl;
+                break;
+
+            default:
+                cout << "Invalid choice! Please try again." << endl;
         }
 
-    }
-    while(choice!=0);
+    } while (choice != 0);
 
     return 0;
-
-
 }

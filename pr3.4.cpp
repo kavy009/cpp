2f3fc7048b4338d557a1a860f148f1f756db92ca
kavy009 <chauhankavya9116@gmail.com>
2025-04-13 18:34:29 +0530

    Add files via upload

---
+++ pr3.4.cpp
@@ -0,0 +1,122 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+template <typename T>
+class DataCollection
+{
+    vector<T> data;
+
+    public:
+    void InputData(int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            T temp;
+            cout<<"Enter "<<i+1<<"th value: ";
+            cin>>temp;
+            data.push_back(temp);
+        }
+        cout<<endl;
+    }
+
+    void DisplayData() {
+        cout<<"Displaying data: ";
+        for(T temp : data)
+        {
+            cout<<temp<<" ";
+        }
+        cout<<endl;
+    }
+
+    void GetMax(int n) {
+        int max = data[0];
+        for(int i=1;i<n;i++)
+        {
+            if (data[i] > max)
+            {
+                max = data[i];
+            }
+        }
+        cout<<"Maximum value: "<<max<<endl;
+    }
+
+    void ReverseCollection(int n) {
+        int st = 0, end = data.size() - 1;
+        cout<<"Reversing collection..."<<endl;
+        while (st < end)
+        {
+            T temp = data[st];
+            data[st] = data[end];
+            data[end] = temp;
+
+            st++;
+            end--;
+        }
+
+    }
+};
+
+int main()
+{
+    int DataType, TotalNumber;
+
+    do
+    {
+        cout<<"Enter the Data Type (1-INT :: 2-FLOAT :: 3-CHAR :: 0-EXIT): ";
+        cin>>DataType;
+
+        if (DataType == 0)
+        {
+            cout<<"Exitting...";
+            break;
+        }
+
+
+        cout<<"Enter Total size of input: ";
+        cin>>TotalNumber;
+
+        switch (DataType)
+        {
+        case 1:
+            {
+                DataCollection<int> dci;
+                dci.InputData(TotalNumber);
+                dci.DisplayData();
+                dci.GetMax(TotalNumber);
+                dci.ReverseCollection(TotalNumber);
+                dci.DisplayData();
+            break;
+            }
+
+
+        case 2:
+            {
+                DataCollection<float> dcf;
+                dcf.InputData(TotalNumber);
+                dcf.DisplayData();
+                dcf.GetMax(TotalNumber);
+                dcf.ReverseCollection(TotalNumber);
+                dcf.DisplayData();
+            break;
+            }
+
+        case 3:
+            {
+                DataCollection<char> dcc;
+                dcc.InputData(TotalNumber);
+                dcc.DisplayData();
+                dcc.GetMax(TotalNumber);
+                dcc.ReverseCollection(TotalNumber);
+                dcc.DisplayData();
+            break;
+            }
+
+        default:
+            cout << "Invalid choice!" << endl;
+            break;
+        }
+    } while (DataType != 0);
+
+    return 0;
+}
---
+++ pr3.5.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class SuperDigit
+{
+    string res = "";
+
+public:
+
+    long long GenerateNum(int Number, int k)
+     {
+        for (int i = 0; i < k; i++)
+        {
+            res += to_string(Number);
+        }
+        long long conc_num = stoll(res);
+        return conc_num;
+    }
+
+    int GetSuperNum(long long num)
+    {
+        if (num < 10)
+        {
+            return num;
+        }
+
+        long long sum = 0;
+        while (num > 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        return GetSuperNum(sum);
+    }
+};
+
+int main()
+{
+    int Number, k;
+
+    cout << "Enter the number: ";
+    cin >> Number;
+    cout << "Enter the number of concatenations: ";
+    cin >> k;
+
+    SuperDigit sd;
+    long long GenNum = sd.GenerateNum(Number, k);
+    int res = sd.GetSuperNum(GenNum);
+    cout << "Result: " << res << endl;
+
+    return 0;
+}

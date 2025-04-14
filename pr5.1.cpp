5e39219e227296d280846f2b203bb7168e9347e6
kavy009 <chauhankavya9116@gmail.com>
2025-04-14 16:07:27 +0530

    Update pr5.1.cpp

--- pr5.1.cpp
+++ pr5.1.cpp
@@ -4,40 +4,102 @@ using namespace std;
 class CalculatorSystem
 {
 public:
+
     int add(int a, int b)
     {
         return a + b;
     }
 
-    double add(double a, double b)
+
+    float add(float a, float b)
     {
         return a + b;
     }
 
-    double add(int a, double b)
+    float add(int a, float b)
     {
         return a + b;
     }
 
-    double add(double a, int b)
+
+    float add(float a, int b)
     {
         return a + b;
+    }
+
+    void displayResult(float result)
+    {
+        cout << "Result: " << result << endl;
     }
 };
 
-int main()
- {
-    CalculatorSystem c;
+int main() {
+    CalculatorSystem calc;
 
-    int intResult = c.add(5, 10);
-    double doubleResult = c.add(5.5, 10.5);
-    double mixedResult1 = c.add(5, 10.5);
-    double mixedResult2 = c.add(5.5, 10);
+    int i1, i2;
+    float f1, f2;
+    float result;
+    int choice;
+    do{
 
-    cout << "Integer Addition Result: " << intResult << endl;
-    cout << "Double Addition Result: " << doubleResult << endl;
-    cout << "Mixed Addition Result (int + double): " << mixedResult1 << endl;
-    cout << "Mixed Addition Result (double + int): " << mixedResult2 << endl;
+    cout << "Calculator - Choose input type:\n";
+    cout << "1. int + int\n";
+    cout << "2. float + float\n";
+    cout << "3. int + float\n";
+    cout << "4. float + int\n";
+    cout<<"0 To Exit \n";
+    cout << "Enter your choice: ";
 
+    cin >> choice;
+
+
+
+    switch (choice)
+    {
+        case 1:
+            cout << "Enter two integers: ";
+            cin >> i1 >> i2;
+            result = calc.add(i1, i2);
+            calc.displayResult(result);
+            break;
+
+        case 2:
+            cout << "Enter two floats: ";
+            cin >> f1 >> f2;
+            result = calc.add(f1, f2);
+
+            calc.displayResult(result);
+            break;
+
+        case 3:
+            cout << "Enter an integer and a float: ";
+            cin >> i1 >> f1;
+            result = calc.add(i1, f1);
+
+            calc.displayResult(result);
+            break;
+
+        case 4:
+            cout << "Enter a float and an integer: ";
+            cin >> f1 >> i1;
+            result = calc.add(f1, i1);
+
+            calc.displayResult(result);
+            break;
+
+        case 0:
+            cout<<"Exitting...";
+            break;
+
+        default:
+            cout << "Invalid choice!" << endl;
+            return 1;
+
+
+    }
+    }while(choice!=0);
+
+
+    cout<<"\n24CE017";
     return 0;
 }

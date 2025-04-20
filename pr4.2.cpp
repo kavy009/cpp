0c57c0581be9c8cee9f3c4f35c54090ce6ab7506
kavy009 <chauhankavya9116@gmail.com>
2025-04-20 23:28:34 +0530

    Update pr4.2.cpp

--- pr4.2.cpp
+++ pr4.2.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 class Person
@@ -7,53 +8,90 @@ class Person
     int age;
 
 public:
-
     Person(string Name, int Age)
     {
         name = Name;
         age = Age;
-        }
+    }
 
+    void display()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
 };
 
-class Employee: public Person
+class Employee : public Person
 {
     int Eid;
 
 public:
-
-    Employee(string Name, int Age, int id) :Person(Name,Age)
+    Employee(string Name, int Age, int id) : Person(Name, Age)
     {
-        Eid=id;
+        Eid = id;
+    }
+
+    void display()
+    {
+        Person::display();
+        cout << "Employee ID: " << Eid << endl;
     }
 };
 
-class Manager: public Employee
-{
+class Manager : public Employee
+ {
     string Department;
-public:
 
-    Manager(string Name, int Age, int id,string dept): Employee(Name,Age,id)
+public:
+    Manager(string Name, int Age, int id, string dept) : Employee(Name, Age, id)
     {
-        Department=dept;
+        Department = dept;
+    }
+
+    void display()
+    {
+        Employee::display();
+        cout << "Department: " << Department << endl;
     }
 };
 
 int main()
+
 {
-    string Name,dept;
-    int Age, id;
+     int n;
+    cout << "Enter number of managers: ";
+    cin >> n;
 
-    cout<<"Enter name of manager: ";
-    cin>>Name;
-    cout<<"Enter age of manager: ";
-    cin>>Age;
-    cout<<"Enter employee ID:";
-    cin>>id;
-    cout<<"Enter employee department";
-    cin>>dept;
+    vector<Manager> managerList;
 
-     Manager M;
+    for (int i = 0; i < n; i++)
+    {
+        string name, dept;
+        int age, id;
 
-     M(Name,Age,id,dept);
+        cout << "\nEnter details for Manager " << i + 1 << ":\n";
+        cout << "Name: ";
+        getline(cin, name);
+        cout << "Age: ";
+        cin >> age;
+        cout << "Employee ID: ";
+        cin >> id;
+        cout << "Department: ";
+        getline(cin, dept);
+
+        Manager m(name, age, id, dept);
+        managerList.push_back(m);
+    }
+
+
+    cout << "\n--- Manager Details ---\n";
+    for (size_t i = 0; i < managerList.size(); i++)
+    {
+        cout << "\nManager " << i + 1 << ":\n";
+        managerList[i].display();
+    }
+
+
+    cout<<"\n24CE017";
+    return 0;
 }

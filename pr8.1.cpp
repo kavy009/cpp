240244c572883686a064f44a72d94f04cf63fe7f
kavy009 <chauhankavya9116@gmail.com>
2025-05-06 21:43:02 +0530

    Update pr8.1.cpp

--- pr8.1.cpp
+++ pr8.1.cpp
@@ -16,7 +16,7 @@ int main() {
     int num;
 
     cout << "Enter " << n << " integers:\n";
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; i++) {
         cin >> num;
         sequence.push_back(num);
     }
